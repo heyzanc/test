f5257098fbaa6f3526db298b4fa04945d2c6077f
heyzanc <heyzanc@gmail.com>
2025-09-25 18:33:01 +0700

    async function

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 console.log("Hello world!")
 console.log("Hai Zan!")
+
+async function test() {
+    try {
+        console.log("bymaximise")
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+test().then(console.log(test))
